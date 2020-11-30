bddbb64453433f7f3648304d7d75fbd7d33025fa
guillaume <guillaume.rault@epitech.eu>
2020-11-30 15:21:53 +0100

    refacto - routes

--- Front/src/router/index.ts
+++ Front/src/router/index.ts
@@ -1,33 +1,19 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from '../views/Home.vue'
-import Dashboard from '../components/Dashboard.vue'
-import Plannings from '../components/Plannings.vue'
-import TicketList from '../components/TicketList.vue'
+import store from "../store/index";
+import { routes } from "./routes";
 
 Vue.use(Router);
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
-    },
-    {
-      path: '/dashboard',
-      name: 'Dashboard',
-      component: Dashboard
-    },
-    {
-      path: '/plannings',
-      name: 'Plannings',
-      component: Plannings
-    },
-    {
-      path: '/tickets',
-      name: 'TicketList',
-      component: TicketList
-    }
-  ]
+const router = new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+});
+
+router.beforeEach((to, from, next) => {
+    next();
 })
+
+
+export default router;
