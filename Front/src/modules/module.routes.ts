094df7320fe764685dc440b83405a871cb7acc75
guillaume <guillaume.rault@epitech.eu>
2020-11-30 15:21:53 +0100

    refacto - routes

--- Front/src/modules/module.routes.ts
+++ Front/src/modules/module.routes.ts
@@ -1,11 +1,15 @@
 import { RouteConfig } from "vue-router";
+import AuthRoute from "./authentification/module.routes";
 
 const TicketList = (resolve: any) => require(["@/components/TicketList.vue"], (m: any) => resolve(m.default));
 
 
-const module: RouteConfig = {
-    path: '/',
-    component: TicketList,
-};
+const module: RouteConfig[] = [
+    {
+        path: '/',
+        component: TicketList,
+    },
+    ...AuthRoute
+];
 
 export default module;
