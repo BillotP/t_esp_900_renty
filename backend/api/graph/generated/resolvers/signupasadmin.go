285e766d7d81b6a58f0d3cb80832d425e936749b
remi219 <remi.correnson@epitech.eu>
2021-01-25 17:39:33 +0100

    fix test signup admin

--- backend/api/graph/generated/resolvers/signupasadmin.go
+++ backend/api/graph/generated/resolvers/signupasadmin.go
@@ -26,7 +26,7 @@ func (r *MutationResolver) SignupAsAdmin(ctx context.Context, input models.Admin
 			Role:     models.RoleAdmin,
 		},
 	}
-	if err = r.DB.Where("user_id = ?", admin.User.ID).First(&admin).Error; err == nil {
+	if err = r.DB.Where("UserID = ?", admin.UserID).First(&admin).Error; err == nil {
 		return nil, fmt.Errorf("admin seems already register")
 	}
 	if pwdHash, err = bcrypt.GenerateFromPassword([]byte(input.User.Password), getPseudoRandomCost()); err != nil {
