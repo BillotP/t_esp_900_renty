a879f102feaf830885a006c1742bfb87b1b8f52a
grault <guillaume.rault@epitech.eu>
2021-02-15 16:33:18 +0100

    ticketlist

--- Front/src/modules/ticketList/TicketList.vue
+++ Front/src/modules/ticketList/TicketList.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
-    <h1>Test</h1>
-    <p>{{ test() }}</p>
+    <h1>t</h1>
     <ticketlist-field />
   </div>
 </template>
@@ -13,48 +12,5 @@ import gql from "graphql-tag";
 
 @Component
 export default class TicketList extends Vue {
-  // private async loginAsCompany() {
-  //   console.log("coucou");
-  //   const result = await this.$apollo.mutate({
-  //     // Query
-  //     mutation: gql`
-  //       mutation($input: String!) {
-  //         addTag(input: $input) {
-  //           input
-  //         }
-  //       }
-  //     `,
-  //     // Parameters
-  //     variables: {
-  //       input: {username : "test", password: "test"},
-  //     },
-  //   });
-  // }
-
-  private test() {
-  return gql`query Tenants {
-  tenants(where: { is_public: { _eq: false} }, order_by: { created_at: desc }) {
-    ID
-    createdAt
-    updatedAt
-    properties
-    userID
-    user
-    estateAgentID
-    EstateAgent
-    documents
-  }
- }`;
-}
-
-  // public mounted() {
-  //   apollo: {
-  //     tenants: gql`
-  //       query {
-  //         tenants: hello
-  //       }
-  //     `;
-  //   }
-  // }
 }
 </script>
