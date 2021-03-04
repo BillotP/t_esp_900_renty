b565cb6f891b3c18dbe285d29b89d95eb8e9b527
grault <guillaume.rault@epitech.eu>
2021-03-04 15:48:37 +0100

    feature - form create property

--- frontend/src/modules/estateAgent/property/CreateProperty.vue
+++ frontend/src/modules/estateAgent/property/CreateProperty.vue
@@ -1,7 +1,5 @@
 <template>
   <v-form v-model="valid">
-    <h1>{{this.password}} = {{ this.repeatPassword}}</h1>
-    <h1>{{passwordRules}}</h1>
     <v-container>
       <v-row>
         <h1>Create Property</h1>
@@ -21,6 +19,10 @@
         <v-col cols="12" md="4">
           <v-text-field
             v-model="password"
+            :error-messages="
+              this.password === this.repeatPassword ? '' : 'Password must match'
+            "
+            :rules="passwordRules"
             label="password"
             required
           ></v-text-field>
@@ -28,9 +30,21 @@
         <v-col cols="12" md="4">
           <v-text-field
             v-model="repeatPassword"
-            :rules="passwordRules"
             label="Repeat password"
+            :rules="passwordRules"
             required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" md="4">
+          <v-text-field
+            v-model="city"
+            append-outer-icon="mdi-map"
+            menu-props="auto"
+            hide-details
+            label="Estate City"
+            single-line
           ></v-text-field>
         </v-col>
       </v-row>
@@ -44,19 +58,28 @@
           ></v-text-field>
         </v-col>
       </v-row>
-
       <v-row>
-        <v-select
-          v-model="value"
-          :items="items"
-          chips
-          label="Chips"
-          multiple
-          outlined
-        ></v-select>
+        <v-col cols="12" md="4">
+          <v-select
+            v-model="value"
+            :items="items"
+            chips
+            label="Chips"
+            multiple
+            outlined
+            solo
+            dense
+          ></v-select>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-hover>
+            <v-btn outlined @click="CreateEstateAgent" :disabled="!(password === repeatPassword && password.length > 0)">Create</v-btn>
+          </v-hover>
+        </v-col>
       </v-row>
     </v-container>
-    <v-btn>qsdsqd</v-btn>
   </v-form>
 </template>
 
@@ -69,6 +92,7 @@ export default class CreateProperty extends Vue {
   firstname = "";
   password = "";
   repeatPassword = "";
+  city = "";
   valid = false;
   nameRules = [
     (v) => !!v || "Name is required",
@@ -80,13 +104,17 @@ export default class CreateProperty extends Vue {
     (v) => !!v || "E-mail is required",
     (v) => /.+@.+/.test(v) || "E-mail must be valid",
   ];
-  passwordRules = [
-    (v) => !!v || "Password is required",
-    this.password === this.repeatPassword || "Password must match",
-  ];
   items = ["foo", "bar", "fizz", "buzz"];
   value = [];
+  passwordRules = [(v) => !!v || "Password is required"];
+
+  CreateEstateAgent()
+  {
+    console.debug(this.firstname, "  " + this.password);
+  }
+
 }
+
 </script>
 
 <style>
