ccf0cc969b00b07342767ec74671bcb150f0e8b6
Migz3rrr <miguel.joubert@epitech.eu>
2021-03-05 15:00:27 +0100

    fix(#51): fix bug on property tenant link

--- backend/api/graph/generated/resolvers/updatetenantprofile.go
+++ backend/api/graph/generated/resolvers/updatetenantprofile.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/BillotP/t_esp_900_renty/v2/backend/api/graph/generated/models"
 	"github.com/BillotP/t_esp_900_renty/v2/backend/api/graph/lib"
+	"gorm.io/gorm"
 )
 
 // UpdateTenantProfile udpate a tenant item in db based on input datas
@@ -17,27 +18,37 @@ func (r *MutationResolver) UpdateTenantProfile(ctx context.Context, input *model
 
 	username := ctx.Value(lib.ContextKey("username")).(string)
 	tenant = models.Tenant{User: &models.User{Username: username}}
-	if err = r.DB.First(&tenant).Error; err != nil {
+	if err = r.DB.Joins("User").Where("username = ?", tenant.User.Username).First(&tenant).Error; err != nil {
 		lib.LogError("mutation/UpdateTenantProfile", err.Error())
 		return nil, err
 	}
 	for el := range input.Properties {
 		if input.Properties[el] != nil {
-			nprops = append(nprops, &models.Property{
+			property := &models.Property{
 				ID: input.Properties[el],
-			})
+			}
+			if err = r.DB.Where("id = ?", input.Properties[el]).First(&property).Error; err != nil {
+				lib.LogError("mutation/UpdateTenantProfile", err.Error())
+				return nil, err
+			}
+			nprops = append(nprops, property)
 		}
 	}
 	for el := range input.Documents {
 		if input.Documents[el] != nil {
-			ndocs = append(ndocs, &models.Asset{
-				ID: input.Properties[el],
-			})
+			document := &models.Asset{
+				URL: *input.Documents[el],
+			}
+			if err = r.DB.Where("url = ?", input.Documents[el]).First(&document).Error; err != nil {
+				lib.LogError("mutation/UpdateTenantProfile", err.Error())
+				return nil, err
+			}
+			ndocs = append(ndocs, document)
 		}
 	}
 	tenant.Documents = ndocs
 	tenant.Properties = nprops
-	if err = r.DB.Updates(&tenant).Error; err != nil {
+	if err = r.DB.Session(&gorm.Session{FullSaveAssociations: true}).Updates(&tenant).Error; err != nil {
 		lib.LogError("mutation/UpdateTenantProfile", err.Error())
 		return nil, err
 	}
