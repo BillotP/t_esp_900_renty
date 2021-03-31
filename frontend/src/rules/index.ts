d7f25eae89ce699e3197f7c02fbb0566c38be58e
dave-lopeur <lopeurd@gmail.com>
2021-03-31 11:03:40 +0200

    chore(fontend/garbage): Cleaning scaffold / Remove #jecopiesanssavoir garbage

--- frontend/src/rules/index.ts
+++
@@ -1,7 +0,0 @@
-export type Rule = (v: any) => boolean | string;
-export const RequiredRule: Rule = (v: any) => !!v || "Ce champ est obligatoire";
-export const MailFormatRule: Rule = (v: any) =>
-    /.+@.+/.test(v) || "L'email est incorrect";
-export const PasswordRule: Rule = (v: any) =>
-    (v && /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*(),.?":{}|<>])[0-9a-zA-Z!@#$%^&*(),.?":{}|<>]{8,}$/.test(v))
-    || "Le mot de passe doit avoir au moins 8 caratères dont une majuscule, un chiffre et un caractère spécial";
--- frontend/src/services/types.ts
+++
@@ -1,4 +0,0 @@
-export interface AuthentificationRequest {
-  email: string;
-  password: string;
-}
