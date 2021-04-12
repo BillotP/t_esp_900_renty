643b83de622f5063f47c10e657f464e0744fbde1
dave-lopeur <lopeurd@gmail.com>
2021-04-12 12:56:05 +0200

    fix(frontend): Update properties var

--- frontend/src/modules/tenant/Profile.vue
+++ frontend/src/modules/tenant/Profile.vue
@@ -1,13 +1,12 @@
 <template>
   <v-container>
-    <v-card
-        v-if="tenant && tenant.user && tenant.user.username"
-        tile
-    >
+    <v-card v-if="tenant && tenant.user && tenant.user.username" tile>
       <v-list>
         <v-list-item>
           <v-list-item-avatar>
-            <v-img src="https://www.flaticon.com/premium-icon/icons/svg/1018/1018651.svg"></v-img>
+            <v-img
+              src="https://www.flaticon.com/premium-icon/icons/svg/1018/1018651.svg"
+            ></v-img>
           </v-list-item-avatar>
         </v-list-item>
 
@@ -20,13 +19,8 @@
         </v-list-item>
       </v-list>
       <v-divider></v-divider>
-      <v-list
-      >
-        <v-list-group
-            no-action
-            sub-group
-            prepend-icon="mdi-home"
-        >
+      <v-list>
+        <v-list-group no-action sub-group prepend-icon="mdi-home">
           <template v-slot:activator>
             <v-list-item-content>
               <v-list-item-title>Properties</v-list-item-title>
@@ -34,22 +28,19 @@
           </template>
 
           <v-list-item
-              v-for="(property, i) in tenant.properties"
-              v-on:click="goToProperty(property)"
-              :key="i"
-              link
+            v-for="(property, i) in tenant.properties"
+            v-on:click="goToProperty(property)"
+            :key="i"
+            link
           >
             <v-list-item-title v-text="property.address"></v-list-item-title>
-            <v-list-item-subtitle v-text="property.codeNumber"></v-list-item-subtitle>
-
+            <v-list-item-subtitle
+              v-text="property.codeNumber"
+            ></v-list-item-subtitle>
           </v-list-item>
         </v-list-group>
 
-        <v-list-group
-            no-action
-            sub-group
-            prepend-icon="mdi-file-document"
-        >
+        <v-list-group no-action sub-group prepend-icon="mdi-file-document">
           <template v-slot:activator>
             <v-list-item-content>
               <v-list-item-title>Documents</v-list-item-title>
@@ -57,17 +48,15 @@
           </template>
 
           <v-list-item
-              v-for="(document, i) in tenant.documents"
-              v-on:click="downloadDocument(document)"
-              :key="i"
-              link
+            v-for="(document, i) in tenant.documents"
+            v-on:click="downloadDocument(document)"
+            :key="i"
+            link
           >
             <v-list-item-title v-text="document.type"></v-list-item-title>
             <v-list-item-subtitle v-text="document.url"></v-list-item-subtitle>
-
           </v-list-item>
         </v-list-group>
-
       </v-list>
     </v-card>
   </v-container>
@@ -90,7 +79,7 @@ const TENANT_QUERY = gql`
         ID
         area
         address
-        codeNumber
+        postalCode
       }
       documents {
         url
@@ -105,20 +94,24 @@ export default class TenantProfile extends Vue {
   public tenant: any = {};
 
   beforeMount() {
-    this.$apollo.getClient().query({
-      query: TENANT_QUERY,
-      variables: {id: this.$route.params.id}
-    }).then((res) => {
-      this.tenant = res.data.tenant;
-      console.log(res);
-    }).catch((err) => {
-      console.error(err);
-    });
+    this.$apollo
+      .getClient()
+      .query({
+        query: TENANT_QUERY,
+        variables: { id: this.$route.params.id },
+      })
+      .then((res) => {
+        this.tenant = res.data.tenant;
+        console.log(res);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   }
 
   downloadDocument(myDocument: any) {
     const a = document.createElement("a");
-    a.href = 'http://localhost:8080' + myDocument.url;
+    a.href = "http://localhost:8080" + myDocument.url;
     a.target = "_blank";
     document.body.appendChild(a);
     a.click();
@@ -127,10 +120,9 @@ export default class TenantProfile extends Vue {
   }
 
   goToProperty(property: any) {
-    this.$router.push('/property/' + property.ID);
+    this.$router.push("/property/" + property.ID);
   }
 }
-
 </script>
 
 <style>
