da5febbce4c22975e1f827ce0d6ec9dbf983262f
dave-lopeur <lopeurd@gmail.com>
2021-04-19 08:47:43 +0200

    refacto(infra/backup): Puting variables with explicit name + make script executable + wondering where PGPASSWORD is set

--- infra/gcp/scripts/backup.sh
+++ infra/gcp/scripts/backup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+DB_USER=jcatltbukgvvhp
+DB_NAME=d9o2hvngiaghkt
+BACKUP_DIR="/srv/backups/list/backup_d9o2hvngiaghkt_$(date +"%Y-%m-%d_%H-%M-%S")"
+LOCAL_BACKUP_DIR="./backup_d9o2hvngiaghkt_$(date +"%Y-%m-%d_%H-%M-%S")"
+# Exit when anything failed
+set -e
 # Dump database
-pg_dump -h ec2-54-155-87-214.eu-west-1.compute.amazonaws.com -p 5432 -U jcatltbukgvvhp d9o2hvngiaghkt > "/srv/backu
-ps/list/backup_d9o2hvngiaghkt_$(date +"%Y-%m-%d_%H-%M-%S")"
+pg_dump -h ec2-54-155-87-214.eu-west-1.compute.amazonaws.com -p 5432 -U $DB_USER $DB_NAME  > $BACKUP_DIR
 # Delete older backups
 find /srv/backups/list/backup_d9o2hvngiaghkt_* -mtime +3 -exec rm {} \;
 # Copy on the GCS bucket
